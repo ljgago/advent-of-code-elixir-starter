62c2bccb937a4926b8f798d784bf156d80fa4717
Leonardo Gago <ljgago@gmail.com>
2021-10-25 13:14:17 -0300

    docs: remove comments in mix deps

--- mix.exs
+++ mix.exs
@@ -21,13 +21,10 @@ defmodule AdventOfCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:httpoison, "~> 1.8"},
       {:castore, "~> 0.1.0"},
       {:mint, "~> 1.0"},
       {:tesla, "~> 1.4"},
       {:ex_doc, "~> 0.25", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
